291a0f6d9b4fc4d45b6e2cafdff413fca112f3ea
Sharath Bhagavatula <skbhagavatula@gmail.com>
2012-08-05 13:50:12 -0400

    background color subtle blue

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,6 +1,6 @@
 var foundersText =
     "<b>Xinran (Leo) Liu</b><br/>Graduated from Cornell University with a major in Chemistry and Chemical Biology.  " +
-        " He is currently a 4th year medical student at the Geisel School of Medicine trying to decide which specialty to go into.  " +
+        " He is currently a 4th year medical student at the Geisel School of Medicine at Dartmouth trying to decide which specialty to go into.  " +
         "His interests outside of clinical practice include LEAN process improvement, entrepreneurship, and medical education.  " +
         "In his free time, he enjoys reading, ultimate frisbee, basketball, tennis, ping pong, golf, drawing, among numerous other random things." +
         "<br/>" +
@@ -10,9 +10,9 @@ var foundersText =
         "In her free time she enjoys traveling, cooking, playing tennis and golf." +
         "<br/>" +
         "<b>Sharath Bhagavatula</b><br/>" +
-        "Graduated from Cornell University with a major in Electrical and Computer Engineering. He is currently a 4th year medical student " +
-        "at NYU School of Medicine interested in radiology. Outside of the clinic, his academic interests include robotics, engineering " +
-        "research, and making websites. Other interests include tennis, basketball, and watching TV.";
+        "Graduated from Cornell University with a degree in Electrical and Computer Engineering. He's currently a 4th year medical student " +
+        "at NYU School of Medicine, interested in radiology. Outside of the clinic, his academic interests include robotics, engineering " +
+        "research, and website development. Other interests include tennis, basketball, and watching TV.";
 
 
 var collaborationText = "This site is a collaboration between UndergroundMed, a team of medical students, and <a href='#' onclick='openMeduWindow()'>MedU</a>, " +
@@ -20,8 +20,8 @@ var collaborationText = "This site is a collaboration between UndergroundMed, a 
     "written and produced by UndergroundMed; the web site is hosted and maintained through financial and technical support from MedU in support of UndergroundMed's " +
     "novel approach to teaching and building a community of student medical educators.";
 
-var missionStatement ="Our goal is to use brief, practical, high-yield videos to make medical education more accessible and understandable for everyone. " +
-    "Students create all content on UndergroundMed, so information is tailored to what is most important for a medical student and presented in a way that is easy to understand. " +
+var missionStatement ="Our goal is to use brief, practical, high-yield videos to make medical education more accessible and understandable for students. " +
+    "All content on UndergroundMed is created by medical students, so information is tailored to what is most important and presented in a way that is easy to understand. " +
     "Videos are checked for factual correctness by experienced and accomplished medical educators. " +
     "<p/>" +
     "We are, and always intend to be, a student run organization - but one that also has support from nationally recognized and " +
