747c213d7e83c286b62fe3bb69c8bba41a04e3bb
rlcohen <rich.cohen@i-intime.org>
2012-08-06 09:39:45 -0400

    fixed statsObject.isPaused do it doesn't expect a value

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,7 @@ require 'rails/test_help'
 require 'minitest/reporters'
 require 'mocha'
 
-MiniTest::Unit.runner = MiniTest::SuiteRunner.new
-if ENV["RM_INFO"] || ENV["TEAMCITY_VERSION"]
-  MiniTest::Unit.runner.reporters << MiniTest::Reporters::RubyMineReporter.new
-elsif ENV['TM_PID']
-  MiniTest::Unit.runner.reporters << MiniTest::Reporters::RubyMateReporter.new
-else
-  MiniTest::Unit.runner.reporters << MiniTest::Reporters::ProgressReporter.new
-end
+MiniTest::Reporters.use!  MiniTest::Reporters::RubyMineReporter.new
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
