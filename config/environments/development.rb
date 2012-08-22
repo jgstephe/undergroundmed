8ba01e3c668388fd7c00f8c615df166dd8770d71
rlcohen <rich.cohen@i-intime.org>
2012-08-22 12:43:50 -0400

    set config.assets.compress = true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,10 +30,10 @@ Undergroundmed::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
